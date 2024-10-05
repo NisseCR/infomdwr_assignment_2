fa891d5fa707ef68b16d9af53a3291e56563ec8e
Nisse <nissehermsen@gmail.com>
2024-10-05 10:33:57 +0200

    Add docs

--- task_2_1.py
+++ task_2_1.py
@@ -12,6 +12,10 @@ pd.set_option('display.width', 1000)
 
 
 def load_data() -> tuple[DataFrame, DataFrame, DataFrame]:
+    """
+    Load the data from the 3 cvs files.
+    :return: 3 dataframes
+    """
     df_acm = pd.read_csv('./data/ACM.csv', header=0)
     df_dblp = pd.read_csv('./data/DBLP2.csv', header=0, encoding='latin1')
     df_mapping = pd.read_csv('./data/DBLP-ACM_perfectMapping.csv', header=0)
@@ -19,6 +23,12 @@ def load_data() -> tuple[DataFrame, DataFrame, DataFrame]:
 
 
 def preprocess(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Set the id columns as the index and preprocess text columns. Preprocessing includes removing whitespaces,
+    converting to lowercase and replacing missing values with the empty word "".
+    :param df: Dataframe containing raw data
+    :return: Dataframe with preprocessed data
+    """
     df = df.set_index('id')
 
     for col in df.columns:
@@ -38,6 +48,12 @@ def preprocess(df: pd.DataFrame) -> pd.DataFrame:
 
 
 def year_sim(s, t) -> int:
+    """
+    Match / mismatch between two year instances. Returns 1 if matched.
+    :param s: Year instance left
+    :param t: Year instance right
+    :return: Match result
+    """
     if s == t:
         return 1
 
@@ -45,6 +61,15 @@ def year_sim(s, t) -> int:
 
 
 def record_sim(r: pd.Series, lev: sm.Levenshtein, jaro: sm.Jaro, aff: sm.Affine) -> pd.Series:
+    """
+    Apply py_stringmatching built-in similarity scores to a single record. Note that venue (using the affine similarity)
+    is not normalised.
+    :param r: data record
+    :param lev: Levenshtein class instance
+    :param jaro: Jaro class instance
+    :param aff: Affine class instance
+    :return: Record including similarity scores per attribute
+    """
     r['sim_title'] = lev.get_sim_score(r['title_x'], r['title_y'])
     r['sim_authors'] = jaro.get_sim_score(r['authors_x'], r['authors_y'])
     r['sim_venue'] = aff.get_raw_score(r['venue_x'], r['venue_y'])
@@ -53,6 +78,15 @@ def record_sim(r: pd.Series, lev: sm.Levenshtein, jaro: sm.Jaro, aff: sm.Affine)
 
 
 def pairwise_comparison(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+    """
+    Cross join the two dataframes and perform pairwise comparison through similarity scores. The venue score
+    (calculated by the affine similarity) is normalised using the min-max formula. Similarity scores are applied in a
+    vectorised method to increase performance for the O(n^2) algorithm.
+    Finally, all the attribute scores are aggregated using custom weights. The resulting score has a range from 0 to 1.
+    :param df1: Dataframe left
+    :param df2: Dataframe right
+    :return: Cross-joined data with similarity score between records
+    """
     # Reset ids
     df1 = df1.reset_index(names='id')
     df2 = df2.reset_index(names='id')
@@ -77,12 +111,14 @@ def pairwise_comparison(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def read_mapping() -> pd.DataFrame:
-    df = pd.read_csv('./data/DBLP-ACM_perfectMapping.csv', header=0)
-    return df
-
-
 def join_mapping(df_sims: pd.DataFrame, df_mapping: pd.DataFrame) -> pd.DataFrame:
+    """
+    Join the results of similar records to the true duplicate record data. A 'match' column is added to indicate whether
+    a match was correct.
+    :param df_sims: Dataframe containing similarity scores of cross-join
+    :param df_mapping: Dataframe containing the true duplicate records
+    :return: Dataframe with indicator of correct results
+    """
     # Retrieve ids only.
     df_sims = df_sims[['id_x', 'id_y']].rename(columns={'id_x': 'idDBLP', 'id_y': 'idACM'})
     print(df_sims.head())
